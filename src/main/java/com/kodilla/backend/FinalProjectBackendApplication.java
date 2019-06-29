6cffc009bdddc8eb132978e6813d7be9dd9ac06e
GreedFire <greedofiro@gmail.com>
2019-06-29 19:01:53 +0200

    First commit

---
+++ src/main/java/com/kodilla/backend/FinalProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectBackendApplication.class, args);
+	}
+
+}
